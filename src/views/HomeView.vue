c0a700d7bd48d134eeb73780e4b0fde466c986d1
OlivMagno <olivmagno1@gmail.com>
2022-08-21 13:49:42 -0300

    layout design

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="websiteContainer">
+    <div class="logoContainer">
+      <h1>Arquimovelaria</h1>
+    </div>
+    <div class="introContainer">
+      <div class="textContainer">
+        <p>
+          Somos uma empresa formada por arquitetos prontos para tornar seus
+          sonhos realidade, trazendo através dos móveis planejados o conforto e
+          a funcionalidade que o seu lar precisa.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
-  },
 };
 </script>

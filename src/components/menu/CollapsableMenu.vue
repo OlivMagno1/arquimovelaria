cd994266450d8a4d3b2b59deae33815fd74bbab2
OlivMagno <olivmagno1@gmail.com>
2022-08-22 17:12:34 -0300

    fixed collapsable menu closing with event

---
+++ src/components/menu/CollapsableMenu.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="collapsableMenuContainer">
+    <nav>
+      <router-link to="/">Quem somos</router-link>
+      <router-link to="/projetos">Nossos projetos</router-link>
+      <router-link to="/faleconosco">Fale Conosco</router-link>
+      <button @click="callParent">
+        <i class="fa-solid fa-xmark"></i>
+      </button>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CollapsableMenu",
+  setup(props, { emit }) {
+    const callParent = () => {
+      emit("invertShow");
+    };
+
+    return { callParent };
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  position: relative;
+  right: 0;
+  top: 50vh;
+  height: 110vh;
+  width: 15vw;
+
+  background-color: #000;
+}
+
+nav a {
+  text-decoration: none;
+  font-weight: 400;
+  color: #fff;
+  margin: 2rem 1rem;
+  transition: 0.2s;
+
+  display: inline-block;
+  position: relative;
+}
+
+nav a:after {
+  content: "";
+  position: absolute;
+  width: 100%;
+  transform: scaleX(0);
+  height: 1px;
+  bottom: 0;
+  left: 0;
+  background-color: #fff;
+  transform-origin: bottom right;
+  transition: transform 0.15s ease-out;
+}
+
+nav a:hover:after {
+  transform: scaleX(1);
+  transform-origin: bottom left;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+nav a.router-link-exact-active:after {
+  background-color: #42b983;
+}
+</style>
--- src/components/menu/MenuStructure.vue
+++ src/components/menu/MenuStructure.vue
@@ -1,13 +1,33 @@
 <template>
   <div class="menuContainer">
     <h1>Arquimovelaria</h1>
-    <button><i class="fa-solid fa-bars"></i></button>
+    <button @click="changeShow">
+      <i class="fa-solid fa-bars"></i>
+    </button>
+    <Transition name="slide">
+      <CollapsableMenu @invertShow="changeShow" v-show="show" />
+    </Transition>
+    <div class="outsideMenu" @click="changeShow" v-if="show" />
   </div>
 </template>
 
 <script>
+import CollapsableMenu from "./CollapsableMenu.vue";
 export default {
   name: "MenuStructure",
+  components: {
+    CollapsableMenu,
+  },
+  data() {
+    return {
+      show: false,
+    };
+  },
+  methods: {
+    changeShow() {
+      this.show = !this.show;
+    },
+  },
 };
 </script>
 
@@ -17,7 +37,7 @@ export default {
   top: 0;
   left: 0;
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-end;
   align-items: center;
 
   width: 100vw;
@@ -25,7 +45,8 @@ export default {
 }
 
 h1 {
-  margin: 0 3rem;
+  position: fixed;
+  left: 3rem;
   color: #fff;
 
   cursor: pointer;
@@ -33,6 +54,11 @@ h1 {
 }
 
 button {
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
   background-color: rgba(0, 0, 0, 0);
   color: #fff;
 
@@ -40,5 +66,28 @@ button {
   cursor: pointer;
   margin: 0 3rem;
   font-size: 1.6rem;
+}
+
+.outsideMenu {
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  width: 85vw;
+  height: 100vh;
+}
+
+.slide-leave-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-enter-active {
+  transition: all 0.3s ease-out;
+  transition-delay: 0.5s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateX(20vw);
 }
 </style>

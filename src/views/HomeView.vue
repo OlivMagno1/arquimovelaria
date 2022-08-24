622a0ef4a28504dd41be4aaf94c148abad1e73f4
OlivMagno <olivmagno1@gmail.com>
2022-08-24 01:04:55 -0300

    added footer to page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,20 @@
   <div class="websiteContainer">
     <IntroductionSection />
     <PortfolioSection />
+    <FooterSection />
   </div>
 </template>
 
 <script>
 import IntroductionSection from "@/components/bodySections/IntroductionSection.vue";
 import PortfolioSection from "@/components/bodySections/PortfolioSection.vue";
+import FooterSection from "@/components/footer/FooterSection.vue";
 export default {
   name: "HomeView",
   components: {
     IntroductionSection,
     PortfolioSection,
+    FooterSection,
   },
 };
 </script>

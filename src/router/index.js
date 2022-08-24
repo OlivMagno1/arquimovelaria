87d9325dcb7e2a85ea5e176d0f50b8d3f6e64824
OlivMagno <olivmagno1@gmail.com>
2022-08-23 23:53:53 -0300

    removed unused views

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import ProjetosView from "../views/ProjetosView.vue";
-import FaleConoscoView from "../views/FaleConoscoView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
-  },
-  {
-    path: "/projetos",
-    name: "projetos",
-    component: ProjetosView,
-  },
-  {
-    path: "/faleconosco",
-    name: "faleconosco",
-    component: FaleConoscoView,
   },
 ];
 

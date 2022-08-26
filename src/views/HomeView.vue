ef4dbb736e8ec3ca1e48f3be1b5bcf98a48ee76e
OlivMagno <olivmagno1@gmail.com>
2022-08-26 16:32:43 -0300

    added contact section to view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
   <div class="websiteContainer">
     <IntroductionSection />
     <PortfolioSection />
+    <ContactSection />
     <FooterSection />
   </div>
 </template>
@@ -10,12 +11,14 @@
 import IntroductionSection from "@/components/bodySections/IntroductionSection.vue";
 import PortfolioSection from "@/components/bodySections/PortfolioSection.vue";
 import FooterSection from "@/components/footer/FooterSection.vue";
+import ContactSection from "../components/bodySections/ContactSection.vue";
 export default {
   name: "HomeView",
   components: {
     IntroductionSection,
     PortfolioSection,
     FooterSection,
+    ContactSection,
   },
 };
 </script>
